b7a6ebad127c336e0f2f59d11b2eeadb61a45dbb
emiling <scopradical@gmail.com>
2021-09-21 01:17:15 +0900

    health check 정상 배포 테스트

--- dgdg-api/src/main/java/com/depromeet/dgdg/controller/BaseResponse.java
+++ dgdg-api/src/main/java/com/depromeet/dgdg/controller/BaseResponse.java
@@ -9,6 +9,7 @@ import lombok.*;
 public class BaseResponse<T> {
 
     public static final BaseResponse<String> OK = success("OK");
+    public static final BaseResponse<String> HEALTH_CHECK = success("득근득근 API 서버");
 
     private String code;
 
@@ -17,7 +18,7 @@ public class BaseResponse<T> {
     private T data;
 
     public static <T> BaseResponse<T> success(T data) {
-        return new BaseResponse<>("", "", data);
+        return new BaseResponse<>("0000", "성공", data);
     }
 
 }
