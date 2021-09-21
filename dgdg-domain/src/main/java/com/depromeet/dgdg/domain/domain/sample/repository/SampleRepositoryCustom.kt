3c526f8f014328ae998a4910b996bc139ae828c6
seungh0 <will.seungho@gmail.com>
2021-09-21 23:46:10 +0900

    fix: 엔티티 클래스를 자바로만 작성하기로 함으로써 QueryDSL 설정 변경

--- dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/sample/repository/SampleRepositoryCustom.kt
+++
@@ -1,9 +0,0 @@
-package com.depromeet.dgdg.domain.domain.sample.repository
-
-import com.depromeet.dgdg.domain.domain.sample.Sample
-
-interface SampleRepositoryCustom {
-
-    fun findSampleById(id: Long): Sample?
-
-}
