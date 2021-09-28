7b7351f8304effc1cacb84a8fe9195deb6f8a72e
seungh0 <will.seungho@gmail.com>
2021-09-29 00:39:04 +0900

    fix: 에러 코드 수정

--- dgdg-common/src/main/java/com/depromeet/dgdg/common/ErrorCode.java
+++ dgdg-common/src/main/java/com/depromeet/dgdg/common/ErrorCode.java
@@ -8,9 +8,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorCode {
 
-    UNAUTHORIZED_EXCEPTION(401, "UNAUTHORIZED_EXCEPTION", "잘못된 토큰입니다. 다시 로그인해주세요"),
-    TOKEN_EXPIRED_EXCEPTION(401, "TOKEN_EXPIRED_EXCEPTION", "토큰이 만료되었습니다."),
-    INTERNAL_SERVER_EXCEPTION(500, "INTERNAL_SERVER_EXCEPTION", "서버 내부에서 에러가 발생하였습니다")
+    // UnAuthorized
+    UNAUTHORIZED_EXCEPTION(401, "U001", "잘못된 토큰입니다. 다시 로그인해주세요"),
+    TOKEN_EXPIRED_EXCEPTION(401, "U002", "토큰이 만료되었습니다."),
+
+    // InternalServer
+    INTERNAL_SERVER_EXCEPTION(500, "I001", "서버 내부에서 에러가 발생하였습니다"),
     ;
 
     private final int status;
