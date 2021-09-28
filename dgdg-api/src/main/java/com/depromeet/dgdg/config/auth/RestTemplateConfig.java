642bade51cbe5d9de74698e3d652f9515f23dfc7
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-09-29 03:40:20 +0900

    RestTemplate 빈 등록

---
+++ dgdg-api/src/main/java/com/depromeet/dgdg/config/auth/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package com.depromeet.dgdg.config.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+    }
+}
