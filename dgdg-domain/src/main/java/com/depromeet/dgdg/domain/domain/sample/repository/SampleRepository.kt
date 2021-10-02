497c30ee2d79afa941c501b1598e2360ec24e1e9
seungh0 <will.seungho@gmail.com>
2021-10-02 18:59:19 +0900

    fix: 샘플 코드 제거

--- dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/sample/repository/SampleRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.depromeet.dgdg.domain.domain.sample.repository
-
-import com.depromeet.dgdg.domain.domain.sample.Sample
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SampleRepository : JpaRepository<Sample, Long>, SampleRepositoryCustom {
-
-}
