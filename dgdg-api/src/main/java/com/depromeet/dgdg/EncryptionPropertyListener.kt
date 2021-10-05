9d6751059328e969a5f7533f22f9fa3e0531a39e
Eugene Lee <scopradical@gmail.com>
2021-10-05 21:57:48 +0900

    feat : AWS secrets manager 및 PostgreSQL 셋팅 (#16)
    * aws secrets manager auto configuration 설정
    
    * Component 어노테이션 삭제
    
    * kakao 앱키 AWS secrets manager 추가
    
    * Delete application-oauth.yml
    
    * 으아아악
    
    * CI aws credential 추가

---
+++ dgdg-api/src/main/java/com/depromeet/dgdg/EncryptionPropertyListener.kt
@@ -0,0 +1,64 @@
+package com.depromeet.dgdg
+
+import com.amazonaws.services.secretsmanager.AWSSecretsManager
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.PropertiesPropertySource
+import org.springframework.stereotype.Component
+import java.util.*
+
+class EncryptionPropertyListener() : ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+
+    override fun onApplicationEvent(event: ApplicationEnvironmentPreparedEvent) {
+
+        val environment = event.environment
+        val client = initClient(environment)
+        val secret = getCredential(environment, client)
+
+        val map : HashMap<String, String> = jacksonObjectMapper().readValue(secret)
+
+        environment.propertySources.addFirst(
+            PropertiesPropertySource("awsSecretsProps", setDatabaseCredentials(map)),
+        )
+        environment.propertySources.addAfter(
+            "awsSecretsProps", PropertiesPropertySource("oauthSecretsProps", setOauthCredentials(map))
+        )
+    }
+
+    private fun initClient(environment: ConfigurableEnvironment) : AWSSecretsManager {
+        val region = environment.getProperty(AWS_REGION)
+        return AWSSecretsManagerClientBuilder.standard().withRegion(region).build()
+    }
+
+    private fun getCredential(environment: ConfigurableEnvironment, client: AWSSecretsManager) : String{
+        val secretName = environment.getProperty(AWS_SECRET_NAME)
+        val request = GetSecretValueRequest().withSecretId(secretName)
+        val response = client.getSecretValue(request)
+        return response.secretString ?: String(Base64.getDecoder().decode(response.secretBinary).array())
+    }
+
+    private fun setDatabaseCredentials(map: HashMap<String, String>): Properties {
+        val props = Properties()
+        props["aws.secret-manager.url"] = map["url"]
+        props["aws.secret-manager.username"] = map["username"]
+        props["aws.secret-manager.password"] = map["password"]
+        return props
+    }
+
+    private fun setOauthCredentials(map:HashMap<String, String>): Properties {
+        val props = Properties()
+        props["oauth.kakao.client-id"] = map["kakao"]
+        return props
+    }
+
+    companion object {
+        private const val AWS_SECRET_NAME = "aws.encrypt.secret-name"
+        private const val AWS_REGION = "aws.encrypt.region"
+    }
+
+}
