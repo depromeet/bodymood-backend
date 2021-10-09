f88f56f050c56f111f6deb5e73f85134b9a5e10c
seungh0 <will.seungho@gmail.com>
2021-10-09 21:12:55 +0900

    fix: 컨디션 및 강도에 순서 필드 추가

--- dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/memo/ConditionType.kt
+++ dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/memo/ConditionType.kt
@@ -2,10 +2,11 @@ package com.depromeet.dgdg.domain.domain.memo
 
 // TODO 기획상 정해지면 카테고리 추가
 enum class ConditionType(
-    val description: String
+    val description: String,
+    val order: Int
 ) {
 
-    WORST("매우 나쁨"),
-    BEST("매우 좋음")
+    WORST("매우 나쁨", 10),
+    BEST("매우 좋음" ,70)
 
 }
--- dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/memo/IntensityType.kt
+++ dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/memo/IntensityType.kt
@@ -2,10 +2,12 @@ package com.depromeet.dgdg.domain.domain.memo
 
 // TODO 기획상 정해지면 카테고리 추가
 enum class IntensityType(
-    val description: String
+    val description: String,
+    val order: Int
 ) {
 
-    ROUGHLY("한듯 안한듯"),
-    TO_THE_LIMIT("한계까지")
+    ROUGHLY("한듯 안한듯", 10),
+    TO_THE_LIMIT("한계까지", 20),
+    ;
 
 }
