9873456e058d3f102c464192cbcb6e4fc0485d60
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-10-15 20:09:50 +0900

    Mockito를 사용해서 KakaoLoginService 테스트

--- dgdg-api/src/test/java/com/depromeet/dgdg/service/auth/KakaoLoginServiceTest.java
+++ dgdg-api/src/test/java/com/depromeet/dgdg/service/auth/KakaoLoginServiceTest.java
@@ -1,101 +1,100 @@
 package com.depromeet.dgdg.service.auth;
 
+import com.depromeet.dgdg.common.ErrorCode;
+import com.depromeet.dgdg.common.exception.BadRequestException;
 import com.depromeet.dgdg.domain.domain.user.User;
 import com.depromeet.dgdg.domain.domain.user.repository.UserRepository;
 import com.depromeet.dgdg.external.kakao.KakaoClient;
 import com.depromeet.dgdg.external.kakao.dto.response.KakaoUserResponse;
 import com.depromeet.dgdg.service.auth.dto.request.AuthRequest;
-import org.junit.jupiter.api.AfterEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class KakaoLoginServiceTest {
 
-    private static final String id = "id";
-    private static final String nickname = "nickname";
-    private static final String profileImage = "profileImage";
-    private static final String accessToken = "123";
-
+    @InjectMocks
     private KakaoLoginService kakaoLoginService;
 
-    private static class StubKakaoClient implements KakaoClient {
+    @Mock
+    private KakaoClient kakaoClient;
 
-        @Override
-        public KakaoUserResponse getUserInfo(String accessToken) {
-            return new KakaoUserResponse(id, new KakaoUserResponse.Properties(nickname, profileImage));
-        }
-    }
-
-    @Autowired
+    @Mock
     private UserRepository userRepository;
+
+    private KakaoUserResponse kakaoUserResponse;
+    private User user;
 
     @BeforeEach
     public void setUp() {
-//        KakaoClient kakaoClient  = Mockito.mock(KakaoClient.class);
-//        kakaoLoginService = new KakaoLoginService(kakaoClient, userRepository);
-
-        //mock을 사용하고 싶은데 mock을 사용하면 null에러가 뜹니다...
-        //혹시 저렇게 상속받는 방법 밖에 없을까요..
-        //아  mock을 쓰려면 저 request 요청에 담긴 즉 kakaoclient에 있는걸 그냥 쓰면 되니까
-        // 저렇게 값을 다 넣어줄꺼면 mock말고 저렇게 상속받는게 더 좋은 방법인건가욤...??
-
-        kakaoLoginService = new KakaoLoginService(new StubKakaoClient(), userRepository);
+        kakaoUserResponse = new KakaoUserResponse("socialId", new KakaoUserResponse.Properties("nickname", "profileImage"));
+        user = user.newKaKaoInstance("socialId", "name", "profileUrl");
     }
 
-    @AfterEach
-    public void clear() {
-        userRepository.deleteAll();
+
+    @Test
+    public void handleAuthentication_UserIsAlreadyExist_DontCallSaveMethodAndReturnId() throws NoSuchFieldException, IllegalAccessException {
+        //when
+        when(kakaoClient.getUserInfo(any())).thenReturn(kakaoUserResponse);
+        when(userRepository.findBySocialIdAndSocialProvider(eq("socialId"), any())).thenReturn(Optional.of(user));
+
+
+        Field field = User.class.getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(user, 1L);
+
+        //given
+        Long comparedId = kakaoLoginService.handleAuthentication(new AuthRequest("token"));
+
+        //then
+        verify(userRepository, never()).save(any());
+        Assertions.assertThat(user.getId()).isEqualTo(comparedId);
     }
 
     @Test
-    public void handleAuthentication_UserIsNotMemberFromDB_saveTheMember() {
+    public void signUpUser_UserIsNotOurMember_CallSaveMethod() {
+        //when
+        when(kakaoClient.getUserInfo(any())).thenReturn(kakaoUserResponse);
+        when(userRepository.findBySocialIdAndSocialProvider(eq("socialId"), any())).thenReturn(Optional.empty());
+        when(userRepository.save(any())).thenReturn(new User());
 
         //given
+        kakaoLoginService.handleAuthentication(new AuthRequest("token"));
 
-        //when
-        kakaoLoginService.handleAuthentication(AuthRequest.testInstance(accessToken));
+        verify(userRepository,times(1)).save(any());
 
-        //then
-        List<User> users = userRepository.findAll();
-        assertThat(users.size()).isEqualTo(1);
-        User testUser = users.get(0);
-        User compareUser = User.newKaKaoInstance(id, nickname, profileImage);
-        assertThat(testUser.getSocialId()).isEqualTo(compareUser.getSocialId());
 
-        //assertThat(testUser).isSameAs(compareUser);
-
-        //이렇게 동일성말고 동등성으로 체크하고 싶은데 user를 새로 생성하면 새로운 객체가 돼서 한번에 검증이 안되네요...
-        //하나씩 .getSocialId() 이런식으로 검증해야할까요...리팩토링 할 수 있을 거같은데...
     }
 
     @Test
-    public void signUpUser_UserWantToSignUp_SaveDB() {
-        //given
-        User newUser = User.newKaKaoInstance(id, nickname, profileImage);
+    public void handleAuthentication_KakaoClientErr_DontCallSaveMethodAndFindBySocialIdAndSocialProvider() {
+        when(kakaoClient.getUserInfo(any())).thenThrow(new BadRequestException("잘못된 요청입니다.", ErrorCode.BAD_REQUEST_EXCEPTION));
 
-        //when
-        userRepository.save(newUser);
+        try {
+            kakaoLoginService.handleAuthentication(new AuthRequest("token"));
+            fail();
+        } catch (BadRequestException e) {
 
-        //then
-        List<User> users = userRepository.findAll();
-        assertThat(users.size()).isEqualTo(1);
-        User testUser = users.get(0);
-        assertThat(testUser.getSocialId()).isEqualTo(newUser.getSocialId());
-        assertThat(testUser.getProfileUrl()).isEqualTo(newUser.getProfileUrl());
-
-//        assertThat(testUser).isSameAs(newUser);
-
-        //질문
-        //로그인을 이렇게 구현해도 되는건지, request 요청 자체를 mock으로 받아와서
-        //token에 "토큰받은 암호같이 긴 것" 을 넣고 진짜 돌려 받는지 확인해야하는건지.
+        }
+        verify(userRepository, never()).save(any());
+        verify(userRepository, never()).findBySocialIdAndSocialProvider(anyString(), any());
 
     }
+
+
+
+
 
 }
