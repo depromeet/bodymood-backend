b9bd03a0746560b6d173704eadf861355888aa74
seungh0 <will.seungho@gmail.com>
2021-10-23 16:42:56 +0900

    feat: 감정 그라데이션 색 추가 완료

--- dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/poster/Emotion.kt
+++ dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/poster/Emotion.kt
@@ -7,22 +7,22 @@ enum class Emotion(
     val endColor: String
 ) {
 
-    FURIOUS("Furious", "화난", "#ffffff", "#ffffff"),
-    JITTERY("Jittery", "초조", "#ffffff", "#ffffff"),
-    CHEERFUL("Cheerful", "쾌활", "#ffffff", "#ffffff"),
-    PROUD("Proud", "뿌듯", "#ffffff", "#ffffff"),
-    ANXIOUS("Anxious", "불안", "#ffffff", "#ffffff"),
-    IRRITATED("Irritated", "짜증", "#ffffff", "#ffffff"),
-    JOYFUL("Joyful", "기쁜", "#ffffff", "#ffffff"),
-    THRILIED("Thrilied", "짜릿", "#ffffff", "#ffffff"),
-    SAD("Sad", "슬플", "#ffffff", "#ffffff"),
-    BORED("Bored", "지루", "#ffffff", "#ffffff"),
-    CALM("Calm", "평온", "#ffffff", "#ffffff"),
-    FULFILLED("Fulfilled", "충만", "#ffffff", "#ffffff"),
-    DEPRESSED("Depressed", "우울", "#ffffff", "#ffffff"),
-    FATIGUED("Fatigued", "지친", "#ffffff", "#ffffff"),
-    SLEEPY("Sleepy", "나른", "#ffffff", "#ffffff"),
-    COMFORTABLE("Comfortable", "편안", "#ffffff", "#ffffff"),
+    FURIOUS("Furious", "화난", "#6C0000", "#110202"),
+    JITTERY("Jittery", "초조", "#AA2900", "#AA2900"),
+    CHEERFUL("Cheerful", "쾌활", "#FFE500", "#FFF970"),
+    PROUD("Proud", "뿌듯", "#FFE092", "#FFE092"),
+    ANXIOUS("Anxious", "불안", "#082630", "#51094A"),
+    IRRITATED("Irritated", "짜증", "#AE1111", "#AA4700"),
+    JOYFUL("Joyful", "기쁜", "#FFF615", "#FF7A00"),
+    THRILIED("Thrilied", "짜릿", "#FF2876", "#FB2BFF"),
+    SAD("Sad", "슬플", "#1B1B1B", "#222757"),
+    BORED("Bored", "지루", "#274A26", "#0A2227"),
+    CALM("Calm", "평온", "#BDF4CD", "#C1E5A4"),
+    FULFILLED("Fulfilled", "충만", "#FFAF8F", "#FF7C7C"),
+    DEPRESSED("Depressed", "우울", "#2F3135", "#000000"),
+    FATIGUED("Fatigued", "지친", "#533C32", "#2E3112"),
+    SLEEPY("Sleepy", "나른", "#DED84C", "#A2C04F"),
+    COMFORTABLE("Comfortable", "편안", "#C0F0FF", "#AFD9FF"),
     ;
 
 }
