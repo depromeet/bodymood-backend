361cf670f27b68e42bb02d680cb2bde5b7593b7a
seungh0 <will.seungho@gmail.com>
2021-10-23 15:24:02 +0900

    feat: 운동 카테고리 오타 수정

--- dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/exercise/ExerciseCategory.kt
+++ dgdg-domain/src/main/java/com/depromeet/dgdg/domain/domain/exercise/ExerciseCategory.kt
@@ -7,7 +7,7 @@ enum class ExerciseCategory(
     val isActivated: Boolean
 ) {
 
-    SHOULDER("Shoulder", "어꺠 운동", 1, true),
+    SHOULDER("Shoulder", "어깨 운동", 1, true),
     CHEST("Chest", "가슴 운동", 2, true),
     BACK_WAIST("Back/Waist", "등, 허리", 3, true),
     TRICEPS_BICEPS("Triceps/Biceps", "삼두/이두", 4, true),
