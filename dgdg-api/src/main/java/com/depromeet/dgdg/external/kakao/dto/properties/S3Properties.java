099774763486dbe1acfc1962b44884dc88f6db89
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-10-26 13:29:39 +0900

    refactor : properties 분리

--- dgdg-api/src/main/java/com/depromeet/dgdg/external/kakao/dto/properties/S3Properties.java
+++ dgdg-api/src/main/java/com/depromeet/dgdg/external/kakao/dto/properties/S3Properties.java
@@ -1,2 +1,26 @@
-package com.depromeet.dgdg.external.kakao.dto.properties;public class S3Properties {
+package com.depromeet.dgdg.external.kakao.dto.properties;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@NoArgsConstructor
+public class S3Properties {
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
 }
--- dgdg-api/src/main/java/com/depromeet/dgdg/service/upload/S3Service.java
+++ dgdg-api/src/main/java/com/depromeet/dgdg/service/upload/S3Service.java
@@ -8,36 +8,32 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.depromeet.dgdg.external.kakao.dto.properties.S3Properties;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 
+@AllArgsConstructor
 @Service
 public class S3Service {
     private AmazonS3 amazonS3;
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private S3Properties s3Properties;
 
     @PostConstruct
     public void setS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        AWSCredentials credentials = new BasicAWSCredentials(s3Properties.getAccessKey(), s3Properties.getSecretKey());
 
         amazonS3 = AmazonS3ClientBuilder.standard()
             .withCredentials(new AWSStaticCredentialsProvider(credentials))
-            .withRegion(this.region)
+            .withRegion(s3Properties.getRegion())
             .build();
     }
 
@@ -48,8 +44,8 @@ public class S3Service {
         objectMetadata.setContentType(file.getContentType());
         objectMetadata.setContentLength(file.getSize());
 
-        amazonS3.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), objectMetadata)
+        amazonS3.putObject(new PutObjectRequest(s3Properties.getBucket(), fileName, file.getInputStream(), objectMetadata)
             .withCannedAcl(CannedAccessControlList.PublicRead));
-        return amazonS3.getUrl(bucket, fileName).toString();
+        return amazonS3.getUrl(s3Properties.getBucket(), fileName).toString();
     }
 }
