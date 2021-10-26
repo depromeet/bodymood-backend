3b07f352178a02cb822fb1c6a0f6e110caf2e71e
seungh0 <will.seungho@gmail.com>
2021-10-26 23:31:17 +0900

    fix: 감정 카테고리 API에 key 추가

--- dgdg-api/src/main/java/com/depromeet/dgdg/controller/emotion/dto/response/EmotionResponse.kt
+++ dgdg-api/src/main/java/com/depromeet/dgdg/controller/emotion/dto/response/EmotionResponse.kt
@@ -3,6 +3,7 @@ package com.depromeet.dgdg.controller.emotion.dto.response
 import com.depromeet.dgdg.domain.domain.poster.Emotion
 
 data class EmotionResponse(
+    val type: Emotion,
     val title: String,
     val description: String,
     val startColor: String,
@@ -13,6 +14,7 @@ data class EmotionResponse(
     companion object {
         fun of(emotion: Emotion): EmotionResponse {
             return EmotionResponse(
+                emotion,
                 emotion.title,
                 emotion.description,
                 emotion.startColor,
