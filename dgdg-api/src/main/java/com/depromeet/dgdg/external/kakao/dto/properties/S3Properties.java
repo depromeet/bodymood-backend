920bb93e336a99c282b16eaf9b688c202dbe0de3
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-10-26 21:19:28 +0900

    프로퍼티 수정

--- dgdg-api/src/main/java/com/depromeet/dgdg/external/kakao/dto/properties/S3Properties.java
+++ dgdg-api/src/main/java/com/depromeet/dgdg/external/kakao/dto/properties/S3Properties.java
@@ -4,23 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
 @NoArgsConstructor
+@ConfigurationProperties(prefix = "cloud.aws.s3")
 public class S3Properties {
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
 
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
-    @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
 
 }
