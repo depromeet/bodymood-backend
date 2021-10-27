d4b5115239d2dc5205e4ccb65be1005e42ce02f8
Seungho Kang <will.seungho@gmail.com>
2021-10-27 14:28:41 +0900

    fix: 파일을 오브젝트 스토리지에 업로드시 UUID를 통해 유니크한 키 값을 갖도록 수정 (#35)

--- dgdg-api/src/main/java/com/depromeet/dgdg/service/upload/S3Service.java
+++ dgdg-api/src/main/java/com/depromeet/dgdg/service/upload/S3Service.java
@@ -1,20 +1,24 @@
 package com.depromeet.dgdg.service.upload;
 
-import com.amazonaws.Response;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
-import com.amazonaws.util.IOUtils;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.depromeet.dgdg.external.kakao.dto.properties.S3Properties;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.net.URLEncoder;
 
 @AllArgsConstructor
 @Service
@@ -22,6 +26,7 @@ public class S3Service {
     private AmazonS3 amazonS3;
 
     private S3Properties s3Properties;
+
 
     public String upload(MultipartFile file) throws IOException {
         String fileName = file.getOriginalFilename();
@@ -34,21 +39,4 @@ public class S3Service {
             .withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3.getUrl(s3Properties.getBucket(), fileName).toString();
     }
-
-    public ResponseEntity<byte[]> getObject(String storedFileName) throws IOException{
-        S3Object o = amazonS3.getObject(new GetObjectRequest(s3Properties.getBucket(), storedFileName));
-        S3ObjectInputStream objectInputStream = o.getObjectContent();
-        byte[] bytes = IOUtils.toByteArray(objectInputStream);
-
-        String fileName = URLEncoder.encode(storedFileName, "UTF-8").replaceAll("\\+", "%20");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        httpHeaders.setContentLength(bytes.length);
-        httpHeaders.setContentDispositionFormData("attachment", fileName);
-
-        return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
-    }
-
-
-
 }
