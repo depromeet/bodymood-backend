71448e25ffe840e3275f5d67cab6573cdb500a81
emiling <scopradical@gmail.com>
2021-11-07 15:23:34 +0900

    포스터 전체 조회 시 생성 및 수정 날짜 필드 추가

--- dgdg-api/src/main/java/com/depromeet/dgdg/service/poster/dto/PosterPhotoResponse.kt
+++ dgdg-api/src/main/java/com/depromeet/dgdg/service/poster/dto/PosterPhotoResponse.kt
@@ -1,16 +1,21 @@
 package com.depromeet.dgdg.service.poster.dto
 
 import com.depromeet.dgdg.domain.domain.poster.Poster
+import java.time.LocalDateTime
 
 data class PosterPhotoResponse (
     val photoId: Long,
-    val imageUrl: String
+    val imageUrl: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
 ){
     companion object {
         fun of(poster: Poster): PosterPhotoResponse {
             return PosterPhotoResponse(
                 poster.id,
-                poster.imageUrl
+                poster.imageUrl,
+                poster.createdAt,
+                poster.updatedAt
             )
         }
     }
