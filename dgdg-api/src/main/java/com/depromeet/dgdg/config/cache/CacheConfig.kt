3ab2b4b6a8485cab25a492cb5287736df8cbc631
seungh0 <will.seungho@gmail.com>
2021-11-18 02:32:48 +0900

    fix: M1에서 임베디드 레디스 호환성 문제 해결하기 위해서 로컬에서는 캐시를 사용하지 않도록 하는 방안

--- dgdg-api/src/main/java/com/depromeet/dgdg/config/cache/CacheConfig.kt
+++ dgdg-api/src/main/java/com/depromeet/dgdg/config/cache/CacheConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.CachingConfigurerSupport
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -13,6 +14,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
 
+@Profile("local-pg", "dev", "prod")
 @EnableCaching
 @Configuration
 class CacheConfig(
